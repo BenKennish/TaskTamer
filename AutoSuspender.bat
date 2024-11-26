c91a43f983366df73e75397c6c76cdc8030191c5
Ben Kennish <ben@kennish.net>
2024-11-26 01:07:28 +0000

    Fix batch file when path has space

--- AutoSuspender.bat
+++ AutoSuspender.bat
@@ -4,20 +4,19 @@ setlocal
 rem A wrapper around the PowerShell script because, by default, Windows will
 rem refuse to execute PowerShell scripts directly
 
-if "%~1"=="/nofork" (
+if "%~1"=="/inline" (
     echo Running AutoSuspender in this terminal...
     rem Run the powershell script and pass all arguments
-    rem (except the "/nofork")
+    rem (except the "/inline")
     rem
     rem if we use this mode it allows us to see final output that the script
-    rem might produce, e.g. error msg, however, use of ctrl-C causes an annoying
+    rem might produce, e.g. error msg, however, use of ctrl-C can cause an annoying
     rem "Terminate batch job (Y/N)?" prompt
-    rem start /wait 
-    powershell -ExecutionPolicy Bypass -File %~dp0\AutoSuspender.ps1 %2 %3 %4 %5 %6 %7 %8 %9
+    powershell -ExecutionPolicy Bypass -File "%~dp0\AutoSuspender.ps1" %2 %3 %4 %5 %6 %7 %8 %9
 ) else (
     rem run the powershell script (in a separate window), passing all arguments
-    echo Running AutoSuspender in external window ^(use /nofork to run inline^)...
-    start "" powershell -ExecutionPolicy Bypass -File %~dp0\AutoSuspender.ps1 %*
+    echo Running AutoSuspender in external window ^(use /inline to run inline^)...
+    start "" powershell -ExecutionPolicy Bypass -File "%~dp0\AutoSuspender.ps1" %*
 )
 
 exit /b
