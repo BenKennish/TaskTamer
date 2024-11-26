69238b65905119e95293df502d953956617044b3
Ben Kennish <ben@kennish.net>
2024-11-26 00:57:16 +0000

    -File in .bat gives full path

--- AutoSuspender.bat
+++ AutoSuspender.bat
@@ -12,11 +12,12 @@ if "%~1"=="/nofork" (
     rem if we use this mode it allows us to see final output that the script
     rem might produce, e.g. error msg, however, use of ctrl-C causes an annoying
     rem "Terminate batch job (Y/N)?" prompt
-    powershell -ExecutionPolicy Bypass -File "AutoSuspender.ps1" %2 %3 %4 %5 %6 %7 %8 %9
+    rem start /wait 
+    powershell -ExecutionPolicy Bypass -File %~dp0\AutoSuspender.ps1 %2 %3 %4 %5 %6 %7 %8 %9
 ) else (
     rem run the powershell script (in a separate window), passing all arguments
     echo Running AutoSuspender in external window ^(use /nofork to run inline^)...
-    start "" powershell -ExecutionPolicy Bypass -File "AutoSuspender.ps1" %*
+    start "" powershell -ExecutionPolicy Bypass -File %~dp0\AutoSuspender.ps1 %*
 )
 
 exit /b
